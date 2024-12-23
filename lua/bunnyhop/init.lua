7e4ac9af509bf00f1461283ea18927725fa8084b
PLAZMAMA <maorioriori@gmail.com>
2024-12-22 23:55:09 -0500

    create basic startup function

--- lua/bunnyhop/init.lua
+++ lua/bunnyhop/init.lua
@@ -1,1 +1,19 @@
 -- Init of plugin
+
+local M = {}
+
+---@class bhop.opts
+M.defaults = {
+    ---@type "copilot"
+    provier = "copilot",
+    ---@type string
+    api_key = "",
+}
+
+
+---Setup function
+---@param opts? bhop.opts
+function M.setup(opts)
+    M.config = opts
+end
+return M
