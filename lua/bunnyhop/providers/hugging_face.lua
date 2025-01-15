cafaa3e3c2786df4a0c4f4a3bf062a9154509f06
PLAZMAMA <maorioriori@gmail.com>
2025-01-14 19:53:10 -0500

    format hugging_face provider

--- lua/bunnyhop/providers/hugging_face.lua
+++ lua/bunnyhop/providers/hugging_face.lua
@@ -5,7 +5,7 @@ local M = {}
 ---@param config bhop.config User config. Used to get the api_key for now, mabye more things later.
 ---@param callback function Function that gets called after the request is made.
 function M.get_models(config, callback) --luacheck: no unused args
-    callback({ "Qwen/Qwen2.5-Coder-32B-Instruct" })
+    callback { "Qwen/Qwen2.5-Coder-32B-Instruct" }
 end
 
 ---Completes the given prompt.
@@ -14,8 +14,9 @@ end
 ---@param callback function Function that gets called after the request is made.
 ---@return nil
 function M.complete(prompt, config, callback)
-    local hf_url =
-        "https://api-inference.huggingface.co/models/" .. config.model .. "/v1/chat/completions"
+    local hf_url = "https://api-inference.huggingface.co/models/"
+        .. config.model
+        .. "/v1/chat/completions"
     local request_body = vim.json.encode {
         model = config.model,
         messages = { { role = "user", content = prompt } },
@@ -40,7 +41,10 @@ function M.complete(prompt, config, callback)
             end
             local response = vim.json.decode(result.stdout)
             if response.error ~= nil then
-                bhop_log.notify("Hugging Face Error: '" .. response.error .. "'", vim.log.levels.ERROR)
+                bhop_log.notify(
+                    "Hugging Face Error: '" .. response.error .. "'",
+                    vim.log.levels.ERROR
+                )
                 callback("")
                 return
             end
