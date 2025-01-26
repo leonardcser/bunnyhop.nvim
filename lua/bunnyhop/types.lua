d2e04f04e017d16035ea5439064ddf4e6ce6ee76
PLAZMAMA <maorioriori@gmail.com>
2025-01-26 18:09:34 -0500

    docs: add type annotations

--- lua/bunnyhop/types.lua
+++ lua/bunnyhop/types.lua
@@ -6,3 +6,9 @@
 ---@field model string
 ---@field api_key string
 ---@field max_prev_width number
+
+---Prediction data used by the predict and hop functions.
+---@class bhop.Prediction
+---@field file string
+---@field line number
+---@field column number
