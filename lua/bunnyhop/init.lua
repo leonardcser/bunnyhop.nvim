baada08f0d315ed4c7a742477d4be9be420ffe41
PLAZMAMA <maorioriori@gmail.com>
2025-01-26 18:19:12 -0500

    fix: fix highlight alignment

--- lua/bunnyhop/init.lua
+++ lua/bunnyhop/init.lua
@@ -184,9 +184,9 @@ local function open_preview_win(prediction, max_prev_width) --luacheck: no unuse
     local buf = vim.api.nvim_create_buf(false, true)
     vim.api.nvim_buf_set_lines(buf, 0, 1, false, { pred_line_content })
     local namespace = vim.api.nvim_create_namespace("test")
-    local byte_col = vim.str_byteindex(pred_line_content, vim.fn.min {prediction.column, half_preview_win_width})
+    local byte_col = vim.str_byteindex(pred_line_content, vim.fn.min {prediction.column - 1, half_preview_win_width})
     ---@diagnostic disable-next-line: param-type-mismatch
-    vim.api.nvim_buf_add_highlight(buf, namespace, "Cursor", 0, byte_col - 1, byte_col)
+    vim.api.nvim_buf_add_highlight(buf, namespace, "Cursor", 0, byte_col, byte_col + 1)
     local id =  vim.api.nvim_open_win(buf, false, {
         relative = "cursor",
         row = 1,
